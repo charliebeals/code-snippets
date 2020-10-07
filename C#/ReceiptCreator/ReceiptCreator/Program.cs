1a26b3e4b67637f644cb18b1475acea55df3035c
Charlie Beals <charliebeals@gmail.com>
2020-10-07 07:39:47 -0400

    Add receipt creator program

---
+++ C#/ReceiptCreator/ReceiptCreator/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using static System.Console;
+
+namespace ReceiptCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Welcome to the Receipt Creator");
+
+            // Create instance of Receipt object
+            Receipt thisReceipt = new Receipt();
+            thisReceipt.ReceiptNumber = PromptForInt("the receipt number");
+            thisReceipt.ReceiptDate = PromptForDateTime("the date of purchase");
+            thisReceipt.CustomerNumber = PromptForInt("the customer's id number");
+            thisReceipt.CustomerName = PromptForString("the customer's name");
+            thisReceipt.CustomerAddress = PromptForString("the customer's address");
+            thisReceipt.CustomerPhone = PromptForString("the customer's phone number");
+            thisReceipt.ItemNumber = PromptForInt("the item number");
+            thisReceipt.ItemDescription = PromptForString("the item description");
+            thisReceipt.ItemPrice = PromptForDouble("the item price");
+            thisReceipt.Quantity = PromptForInt("the quantity of the item purchased");
+
+            // Call output method
+            WriteLine(thisReceipt);
+        }
+
+        // Input methods with value checking
+        public static int PromptForInt(string key)
+        {
+            int value = 0;
+            bool enterAgain = true;
+            while (enterAgain == true)
+            {
+                string inValue;
+                Write("Please enter " + key + ": ");
+                inValue = ReadLine();
+                if (int.TryParse(inValue, out value) == false)
+                {
+                    WriteLine("invalid interger value");
+                    enterAgain = true;
+                }
+                else
+                {
+                    enterAgain = false;
+                }
+            }
+            return value;
+        }
+        public static string PromptForString(string key)
+        {
+            Write("Please enter " + key + ": ");
+            string value = ReadLine();
+            return value;
+        }
+        public static double PromptForDouble(string key)
+        {
+            double value = 0;
+            bool enterAgain = true;
+            while (enterAgain == true)
+            {
+                string inValue;
+                Write("Please enter " + key + ": ");
+                inValue = ReadLine();
+                if (double.TryParse(inValue, out value) == false)
+                {
+                    WriteLine("invalid double value");
+                    enterAgain = true;
+                }
+                else
+                {
+                    enterAgain = false;
+                }
+            }
+            return value;
+        }
+        public static DateTime PromptForDateTime(string key)
+        {
+            DateTime value = DateTime.Now;
+            bool enterAgain = true;
+            while (enterAgain == true)
+            {
+                string inValue;
+                Write("Please enter " + key + ": ");
+                inValue = ReadLine();
+                if (DateTime.TryParse(inValue, out value) == false)
+                {
+                    WriteLine("invalid double value");
+                    enterAgain = true;
+                }
+                else
+                {
+                    enterAgain = false;
+                }
+            }
+            return value;
+        }
+    }
+}
---
+++ C#/ReceiptCreator/ReceiptCreator/Receipt.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReceiptCreator
+{
+    class Receipt
+    {
+        // Set members
+        private int receiptNumber;
+        private DateTime receiptDate;
+        private int customerNumber;
+        private string customerName;
+        private string customerAddress;
+        private string customerPhone;
+        private int itemNumber;
+        private string itemDescription;
+        private double itemPrice;
+        private int quantity;
+
+        // Default constructor
+        public Receipt()
+        {
+        }
+
+        // Property accessors
+        #region Property Accessors
+        public int ReceiptNumber
+        {
+            get
+            {
+                return receiptNumber;
+            }
+            set
+            {
+                receiptNumber = value;
+            }
+        }
+        public DateTime ReceiptDate
+        {
+            get
+            {
+                return receiptDate;
+            }
+            set
+            {
+                receiptDate = value;
+            }
+        }
+        public int CustomerNumber
+        {
+            get
+            {
+                return customerNumber;
+            }
+            set
+            {
+                customerNumber = value;
+            }
+        }
+        public string CustomerName
+        {
+            get
+            {
+                return customerName;
+            }
+            set
+            {
+                customerName = value;
+            }
+        }
+        public string CustomerAddress
+        {
+            get
+            {
+                return customerAddress;
+            }
+            set
+            {
+                customerAddress = value;
+            }
+        }
+        public string CustomerPhone
+        {
+            get
+            {
+                return customerPhone;
+            }
+            set
+            {
+                customerPhone = value;
+            }
+        }
+        public int ItemNumber
+        {
+            get
+            {
+                return itemNumber;
+            }
+            set
+            {
+                itemNumber = value;
+            }
+        }
+        public string ItemDescription
+        {
+            get
+            {
+                return itemDescription;
+            }
+            set
+            {
+                itemDescription = value;
+            }
+        }
+        public double ItemPrice
+        {
+            get
+            {
+                return itemPrice;
+            }
+            set
+            {
+                itemPrice = value;
+            }
+        }
+        public int Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+            set
+            {
+                quantity = value;
+            }
+        }
+        #endregion
+
+        // Calculate cost from the item price and quantity
+        public double CalculateCost(double price, int quantity)
+        {
+            double quantityAsDouble = (double)quantity;
+            double totalCost = price * quantityAsDouble;
+            double dollarAmmount = Math.Round(totalCost, 2);
+            return dollarAmmount;
+        }
+
+        // Override ToString() method
+        public override string ToString()
+        {
+            return "\t Customer Information" +
+                "\n\nCustomer Name: " + customerName +
+                "\nCustomer Number: " + customerNumber +
+                "\nCustomer Address: " + customerAddress +
+                "\nCustomer Phone: " + customerPhone +
+                "\n\nTotal Cost: $" + CalculateCost(itemPrice, quantity);
+        }
+    }
+}
