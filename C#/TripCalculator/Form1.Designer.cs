2d156be5a61badb1961376e5364ac927d9bedfec
Charlie Beals <charliebeals@gmail.com>
2020-11-21 18:59:54 -0500

    Add trip calculator

---
+++ C#/TripCalculator/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace TripCalculator
+{
+    partial class FormTripCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblDestination = new System.Windows.Forms.Label();
+            this.lblMiles = new System.Windows.Forms.Label();
+            this.lblGallons = new System.Windows.Forms.Label();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.txtMiles = new System.Windows.Forms.TextBox();
+            this.txtGallons = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(284, 194);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnReset.Location = new System.Drawing.Point(203, 194);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblDestination
+            // 
+            this.lblDestination.AutoSize = true;
+            this.lblDestination.Location = new System.Drawing.Point(50, 41);
+            this.lblDestination.Name = "lblDestination";
+            this.lblDestination.Size = new System.Drawing.Size(81, 13);
+            this.lblDestination.TabIndex = 2;
+            this.lblDestination.Text = "Trip Destination";
+            // 
+            // lblMiles
+            // 
+            this.lblMiles.AutoSize = true;
+            this.lblMiles.Location = new System.Drawing.Point(50, 79);
+            this.lblMiles.Name = "lblMiles";
+            this.lblMiles.Size = new System.Drawing.Size(76, 13);
+            this.lblMiles.TabIndex = 3;
+            this.lblMiles.Text = "Miles Traveled";
+            // 
+            // lblGallons
+            // 
+            this.lblGallons.AutoSize = true;
+            this.lblGallons.Location = new System.Drawing.Point(50, 117);
+            this.lblGallons.Name = "lblGallons";
+            this.lblGallons.Size = new System.Drawing.Size(95, 13);
+            this.lblGallons.TabIndex = 4;
+            this.lblGallons.Text = "Gallons Consumed";
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Location = new System.Drawing.Point(168, 34);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.Size = new System.Drawing.Size(191, 20);
+            this.txtDestination.TabIndex = 5;
+            // 
+            // txtMiles
+            // 
+            this.txtMiles.Location = new System.Drawing.Point(168, 72);
+            this.txtMiles.Name = "txtMiles";
+            this.txtMiles.Size = new System.Drawing.Size(56, 20);
+            this.txtMiles.TabIndex = 6;
+            this.txtMiles.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtGallons
+            // 
+            this.txtGallons.Location = new System.Drawing.Point(168, 110);
+            this.txtGallons.Name = "txtGallons";
+            this.txtGallons.Size = new System.Drawing.Size(56, 20);
+            this.txtGallons.TabIndex = 7;
+            this.txtGallons.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(74, 274);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(35, 13);
+            this.lblOutput.TabIndex = 8;
+            this.lblOutput.Text = "label1";
+            this.lblOutput.Visible = false;
+            // 
+            // FormTripCalculator
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.CancelButton = this.btnReset;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.txtGallons);
+            this.Controls.Add(this.txtMiles);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.lblGallons);
+            this.Controls.Add(this.lblMiles);
+            this.Controls.Add(this.lblDestination);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCalculate);
+            this.ForeColor = System.Drawing.Color.DimGray;
+            this.Location = new System.Drawing.Point(100, 100);
+            this.Name = "FormTripCalculator";
+            this.Text = "Trip Calculator Pro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblDestination;
+        private System.Windows.Forms.Label lblMiles;
+        private System.Windows.Forms.Label lblGallons;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.TextBox txtMiles;
+        private System.Windows.Forms.TextBox txtGallons;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ C#/TripCalculator/Form1.cs
@@ -0,0 +1,78 @@
+﻿/*Info
+ * Programmer: Charlie Beals
+ * Date: 11/21/2020
+ * Purpose: Calculate MPG for trip data
+ * Class: CITP 180
+ * Assignment: Ch 9 - Ex 5
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TripCalculator
+{
+    public partial class FormTripCalculator : Form
+    {
+        public FormTripCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            // Clear all values
+            txtDestination.Text = "";
+            txtGallons.Text = "";
+            txtMiles.Text = "";
+
+            // Hide output
+            lblOutput.Visible = false;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // Check if input is valid
+            double numMiles;
+            double numGallons;
+            bool validMiles = double.TryParse(txtMiles.Text, out numMiles);
+            bool validGallons = double.TryParse(txtGallons.Text, out numGallons);
+
+            // If input is valid, calculate and display MPG
+            if (validMiles == true && validGallons == true && numGallons > 0 && numMiles > 0)
+            {
+                double mpg = numMiles / numGallons;
+
+                mpg = Math.Round(mpg, 1);
+
+                string mpgString = mpg.ToString();
+
+                // Set a default value for the destination if none was given
+                string destinationString;
+                if (txtDestination.Text == "")
+                {
+                    destinationString = "your destination";
+                }
+                else
+                {
+                    destinationString = txtDestination.Text;
+                }
+
+                lblOutput.Text = "Fuel effiency for your trip to " + destinationString + " is " + mpgString + " MPG.";
+                lblOutput.Visible = true;
+            }
+            // If input is invalid, display error message
+            else
+            {
+                lblOutput.Text = "Invalid values entered, please try again";
+                lblOutput.Visible = true;
+            }
+        }
+    }
+}
